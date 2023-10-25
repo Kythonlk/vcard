c931077587d6d5b9d63a2466dd8611c5d2b2cf25
kytho <kythonlk@gmail.com>
2023-10-25 12:31:10 +0400

    check form working with react-hook-form with api key and branch

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,11 +1,14 @@
 import type { APIRoute } from 'astro';
 import { XataClient } from '../../../xata';
 
-const client = new XataClient({ apiKey: import.meta.env.XATA_API_KEY });
+const xata = new XataClient({
+    apiKey: import.meta.env.XATA_API_KEY,
+    branch: import.meta.env.XATA_BRANCH
+});
 
-export const POST: APIRoute = async ({request}) => {
+export const POST: APIRoute = async ({ request }) => {
     const user = await request.json();
-    const createUser = await client.db.Users.create(user);
+    const createUser = await xata.db.Users.create(user);
     return new Response(JSON.stringify(createUser), {
         status: 200,
         headers: {
@@ -24,8 +27,4 @@ export const POST: APIRoute = async ({request}) => {
 //         },
 //     });
 // }
-
-
-
-
 
